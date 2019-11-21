712d8f53a9132486190c03ae775d06c5f4c6adc4
zafaraabid <zafaraabid3295@gmail.com>
2019-11-21 10:11:45 +0530

    functionality: User's firstName should start with Caps and min 3 char

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,3 +2,16 @@
 
 echo "-----UserRegistration-----"
 echo "-----Greeting-----"
+
+read -p "Enter First name :" fname
+
+#fnamePattern="^[A-Z]+[a-zA-Z]+[a-zA-Z]+$"
+fnamePattern="^[A-Z]+[a-z]{2,}$"
+
+if [[ $fname =~ $fnamePattern ]]
+then
+        echo "First Name is valid"
+else
+        echo "First Name is invalid"
+fi
+
