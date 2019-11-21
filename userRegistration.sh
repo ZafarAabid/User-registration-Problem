cd0f5b893a2faa7dfbb8ee91d073d876556759fe
zafaraabid <zafaraabid3295@gmail.com>
2019-11-21 10:32:05 +0530

    functionality: User's lastName should start with Caps and min 3 char

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,10 +3,10 @@
 echo "-----UserRegistration-----"
 echo "-----Greeting-----"
 
+function firstName(){
 read -p "Enter First name :" fname
 
-#fnamePattern="^[A-Z]+[a-zA-Z]+[a-zA-Z]+$"
-fnamePattern="^[A-Z]+[a-z]{2,}$"
+fnamePattern="^[A-Z][a-zA-Z]{2,}$"
 
 if [[ $fname =~ $fnamePattern ]]
 then
@@ -15,3 +15,22 @@ else
         echo "First Name is invalid"
 fi
 
+}
+
+function lastName(){
+read -p "Enter Last name :" lname
+
+lnamePattern="^[A-Z][a-zA-Z]{2,}$"
+
+if [[ $lname =~ $lnamePattern ]]
+then
+        echo "last Name is valid"
+else
+        echo "last Name is invalid"
+fi
+
+}
+
+
+#firstName
+lastName
